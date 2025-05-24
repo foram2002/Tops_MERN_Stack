4017034e58e791ea1b43a1eb78ad39cf211492b6
ForamGoriya <151605073+foram2002@users.noreply.github.com>
2025-05-24 18:49:22 +0530

    Add files via upload

---
+++ Task/Assignment/Web_Design/Nodejs/hello-node/task.js
@@ -0,0 +1,12 @@
+// app.js
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!');
+});
+
+server.listen(3000, () => {
+  console.log('Server running at this port');
+});
